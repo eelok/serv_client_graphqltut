c8236a7e31fca62b4c456d5bfa3f3dca94ba6a82
Maria <eelok@mail.ru>
2021-12-05 12:47:40 +0100

    improve Mutation createJob through input type

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,27 +1,33 @@
-
-const db = require('./db');
+const db = require("./db");
 //todo не пойму что это за root
 ///job: (root, {id}) => db.jobs.get(id),
 const Query = {
-    job: (root, args) => db.jobs.get(args.id),
-    jobs: () => db.jobs.list(),
-    company: (root, {id}) => db.companies.get(id),
+  job: (root, args) => db.jobs.get(args.id),
+  jobs: () => db.jobs.list(),
+  company: (root, { id }) => db.companies.get(id),
+};
+///когда делаю createJob для клиента неудобно солько много аргументов
+// поэтому нужно сделать input
+// const Mutation = {
+//     createJob:(root, {companyId, title, description}) => {
+//          const jobId = db.jobs.create({ companyId, title, description });
+//          return db.jobs.get(jobId);
+//     }
+// }
+const Mutation = {
+  createJob: (root, { input }) => {
+    const jobId = db.jobs.create(input);
+    return db.jobs.get(jobId);
+  },
 };
 
-const Mutation = {
-    createJob:(root, {companyId, title, description}) => {
-        return db.jobs.create({companyId, title, description});
-    }
-}
-
 const Job = {
-    company: (job) => db.companies.get(job.companyId)
-}
+  company: (job) => db.companies.get(job.companyId),
+};
 
 const Company = {
-    jobs: (company) => db.jobs.list()
-    .filter(job => job.companyId === company.id)
-}
-
+  jobs: (company) =>
+    db.jobs.list().filter((job) => job.companyId === company.id),
+};
 
 module.exports = { Query, Job, Company, Mutation };
